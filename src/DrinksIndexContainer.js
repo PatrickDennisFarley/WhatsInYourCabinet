77a6b519bfd3c625dfc12b3b0e86c69575bfaf34
nm357 <n.mikita3@gmail.com>
2017-09-07 18:12:34 -0400

    code styling

--- src/DrinksIndexContainer.js
+++ src/DrinksIndexContainer.js
@@ -43,23 +43,24 @@ class DrinksIndexContainer extends Component {
   }
 
   render() {
-
     let title = this.state.title;
     let category = this.state.category;
     let glass = this.state.glass;
     let instructions = this.state.instructions;
     let image = this.state.image;
-      return(
-        <div>
-          <h1>What's In Your Cabinet?</h1>
-          <Button bsStyle='primary' onClick={this.handleClick}>Get a Random Drink!</Button>
-          <h3>{title}</h3>
-          <h5>Category: {category}</h5>
-          <h5>Glass: {glass}</h5>
-          <p>{instructions}</p>
-          <img className="drinkPic" src={image} alt={title}/>
-        </div>
-      )
+    
+    return(
+      <div>
+        <h1>What's In Your Cabinet?</h1>
+        <Button bsStyle='primary' onClick={this.handleClick}>Get a Random Drink!</Button>
+        <h3>{title}</h3>
+        <h5>Category: {category}</h5>
+        <h5>Glass: {glass}</h5>
+        <p>{instructions}</p>
+        <img className="drinkPic" src={image} alt={title}/>
+      </div>
+    )
   }
 }
+
 export default DrinksIndexContainer;
