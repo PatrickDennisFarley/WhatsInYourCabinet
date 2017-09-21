3685a9fc622fcf695771c40a47bbd3158728cae5
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-09-21 17:13:06 -0400

    added user model tests & email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  validates :username, presence: true
+  validates :email, presence: true, email: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
