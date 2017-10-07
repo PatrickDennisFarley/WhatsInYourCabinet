8d1aca1cb07b9ff91accc4e7af107b0dfa8dd187
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-10-07 15:13:09 -0400

    editing application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(user)
+  custom_posts_path
+  end
 
+  def after_update_path_for(user)
+    custom_posts_path(user)
+  end
+  
   protected
 
   def configure_permitted_parameters
